8630418102b519d278f24ad3939be92db1692c99
BeiyanYunyi <14120445+BeiyanYunyi@users.noreply.github.com>
2025-05-29 16:51:27 +0800

    chore: :art: rename test.py to main.py

---
+++ main.py
@@ -0,0 +1,43 @@
+from lib.draw import (
+    draw_p2_2,
+    draw_p4_1,
+    draw_p4_2,
+    draw_p4_3,
+    draw_p4_4,
+    draw_p4_5a,
+    draw_p4_5b,
+    draw_p4_6,
+    draw_p4_7l1,
+    draw_p4_7l2,
+    draw_p4_8,
+    draw_p4_9,
+    draw_p4_10,
+    draw_p4_11,
+    draw_p4_12,
+)
+from lib import print_chinese_fonts
+import matplotlib.pyplot as plt
+
+
+print_chinese_fonts()
+# 输出可用字体名后，选择一个喜欢的，替换下面的 Heiti TC
+plt.rcParams["font.family"] = ["Heiti TC", "sans-serif"]
+# draw_p2_2()
+# draw_p4_1()
+# draw_p4_2()
+# draw_p4_3()
+# draw_p4_4()
+# draw_p4_5a()
+# draw_p4_5b()
+# draw_p4_6()
+# draw_p4_7l1()
+# draw_p4_7l2("500")
+# draw_p4_7l2("700")
+# draw_p4_7l2("850")
+# draw_p4_7l2("925")
+# draw_p4_8()
+# draw_p4_9()
+# draw_p4_10()
+# draw_p4_11()
+# draw_p4_12()
+# plt.show()
